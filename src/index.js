11fcba8c901c3520f6ca5b1569e9172e517ba9ba
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-05-24 12:28:04 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
+
+import Home from './pages/Home';
 import SidebarLayout from './components/shared/SidebarLayout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <SidebarLayout />
+      <Routes>
+        {/* ✅ Home page NOT inside SidebarLayout */}
+        <Route path="/" element={<Home />} />
+
+        {/* ✅ Everything else gets SidebarLayout */}
+        <Route path="/*" element={<SidebarLayout />} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
