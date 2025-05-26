d67b12bb0a4f6854d1fe2c7e0acc02381c4fd08a
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-05-25 23:22:50 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Header from '../shared/Header';
+import Header from '../components/shared/Header';
 
 const Home = () => {
   return (
