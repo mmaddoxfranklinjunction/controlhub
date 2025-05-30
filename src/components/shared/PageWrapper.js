c12233ca65eefc1250efeb0ee583973cf2abbe76
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-05-30 02:15:46 -0400

    Update PageWrapper.js

--- src/components/shared/PageWrapper.js
+++ src/components/shared/PageWrapper.js
@@ -1,10 +1,18 @@
+// src/components/shared/PageWrapper.js
 import React from 'react';
+import Header from './Header';
 import SidebarLayout from './SidebarLayout';
 
 const PageWrapper = ({ children }) => (
-  <SidebarLayout>
-    {children}
-  </SidebarLayout>
+  <div className="min-h-screen flex flex-col bg-[#F9F9F9]">
+    <Header />
+    <div className="flex flex-1">
+      <SidebarLayout />
+      <main className="flex-1 px-6 py-8 overflow-y-auto">
+        {children}
+      </main>
+    </div>
+  </div>
 );
 
 export default PageWrapper;
