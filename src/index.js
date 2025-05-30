9fb483e0e52fd12a079995d5c18076dc909674b5
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-05-30 01:54:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
-// src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import Home from './pages/Home';
-import ControlPanel from './pages/control-panel/ControlPanel';
-import MarketingPanel from './pages/control-panel/MarketingPanel';
-import OperationsPanel from './pages/control-panel/OperationsPanel';
-import LocationsPanel from './pages/control-panel/LocationsPanel';
-import MenuPanel from './pages/control-panel/MenuPanel';
+import AccountSetup from './pages/account-setup';
+import Login from './pages/login';
 
-import SalesOverview from './pages/analytics/SalesOverview';
-import RevenueRecovery from './pages/analytics/RevenueRecovery';
-import PromotionsReport from './pages/analytics/PromotionsReport';
-import RatingsFeedback from './pages/analytics/RatingsFeedback';
-import SponsoredListingReport from './pages/analytics/SponsoredListingReport';
-import OperationsPerformance from './pages/analytics/OperationsPerformance';
-
-import RecommendationsMarketing from './pages/recommendations/RecommendationsMarketing';
-import RecommendationsOperations from './pages/recommendations/RecommendationsOperations';
-import RecommendationsLocations from './pages/recommendations/RecommendationsLocations';
-import RecommendationsMenu from './pages/recommendations/RecommendationsMenu';
+// Layout (wraps all dashboard pages)
+import SidebarLayout from './components/shared/SidebarLayout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -30,27 +16,13 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        {/* 🏠 Public Home Page - no layout wrapper */}
+        {/* Public Home/Login/Signup pages */}
         <Route path="/" element={<Home />} />
+        <Route path="/account-setup" element={<AccountSetup />} />
+        <Route path="/login" element={<Login />} />
 
-        {/* Dashboard Pages (each has <PageWrapper> inside) */}
-        <Route path="/control-panel" element={<ControlPanel />} />
-        <Route path="/control-panel/marketing" element={<MarketingPanel />} />
-        <Route path="/control-panel/operations" element={<OperationsPanel />} />
-        <Route path="/control-panel/locations" element={<LocationsPanel />} />
-        <Route path="/control-panel/menu" element={<MenuPanel />} />
-
-        <Route path="/analytics/sales" element={<SalesOverview />} />
-        <Route path="/analytics/recovery" element={<RevenueRecovery />} />
-        <Route path="/analytics/promotions" element={<PromotionsReport />} />
-        <Route path="/analytics/ratings" element={<RatingsFeedback />} />
-        <Route path="/analytics/sponsored" element={<SponsoredListingReport />} />
-        <Route path="/analytics/operations" element={<OperationsPerformance />} />
-
-        <Route path="/recommendations/marketing" element={<RecommendationsMarketing />} />
-        <Route path="/recommendations/operations" element={<RecommendationsOperations />} />
-        <Route path="/recommendations/locations" element={<RecommendationsLocations />} />
-        <Route path="/recommendations/menu" element={<RecommendationsMenu />} />
+        {/* All other dashboard pages are handled by SidebarLayout! */}
+        <Route path="/*" element={<SidebarLayout />} />
       </Routes>
     </Router>
   </React.StrictMode>
