d568c7edb65d8d9df0703f9fe65340002fe897d7
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-06-02 08:39:48 -0400

    Update MenuPanel.js

--- src/pages/control-panel/MenuPanel.js
+++ src/pages/control-panel/MenuPanel.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from 'react-router-dom';
 import PageWrapper from '../../components/shared/PageWrapper';
+import FilterBar from '../../components/shared/FilterBar';
 
 export default function MenuPanel() {
   const [showMissing, setShowMissing] = useState(true);
