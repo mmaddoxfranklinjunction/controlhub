912c9c76365ecd2cfb6ff8a1e49872622c11398c
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-06-02 18:29:26 -0400

    Update SponsoredListingReport.js

--- src/pages/analytics/SponsoredListingReport.js
+++ src/pages/analytics/SponsoredListingReport.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageWrapper from '../../components/shared/PageWrapper';
 import FilterBar from '../../components/shared/FilterBar';
 
