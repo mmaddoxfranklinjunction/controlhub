aef42d823b1afe69433acc7ec5db40a3d15e86ef
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-06-04 13:44:24 -0400

    Update LocationsPanel.js

--- src/pages/control-panel/LocationsPanel.js
+++ src/pages/control-panel/LocationsPanel.js
@@ -46,12 +46,12 @@ const LocationsPanel = () => {
       <div className="px-5 py-5">
         {/* Title + Toggle */}
         <div className="flex items-center justify-between mt-0 mb-4">
-          <h1 className="text-2xl font-bold text-[#253847]">Location Controls</h1>
+          <h1 className="text-xl font-bold text-[#253847]">Locations Control</h1>
           <div className="flex bg-[rgba(179,40,45,0.09)] rounded-full w-52 h-8 shadow-inner cursor-pointer text-xs border border-[#b3282d]">
             <button
               className={`flex-1 px-3 py-1 rounded-full transition font-bold
                 ${toggle === "insights" ? "bg-[#b3282d] text-white shadow" : "text-[#b3282d] bg-[rgba(179,40,45,0.09)]"}`}
-              style={{ fontSize: "13px", height: "32px", transition: "all 0.15s" }}
+              style={{ fontSize: "12px", height: "25px", transition: "all 0.25s" }}
               onClick={() => setToggle("insights")}
             >
               Insights
@@ -59,7 +59,7 @@ const LocationsPanel = () => {
             <button
               className={`flex-1 px-3 py-1 rounded-full transition font-bold
                 ${toggle === "controls" ? "bg-[#b3282d] text-white shadow" : "text-[#b3282d] bg-[rgba(179,40,45,0.09)]"}`}
-              style={{ fontSize: "13px", height: "32px", transition: "all 0.15s" }}
+              style={{ fontSize: "12px", height: "25px", transition: "all 0.25s" }}
               onClick={() => setToggle("controls")}
             >
               Controls
