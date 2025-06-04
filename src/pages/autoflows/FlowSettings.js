30976c46329466c8cad11ae13833a1cb5b7332ec
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-06-04 11:45:00 -0400

    Update FlowSettings.js

--- src/pages/autoflows/FlowSettings.js
+++ src/pages/autoflows/FlowSettings.js
@@ -62,7 +62,7 @@ const FlowSettings = () => {
       <div className="px-4 py-2">
         {/* Logo + Title + FilterBar Row */}
         <div className="flex items-center gap-3 mb-4">
-          <img src="/fj-circle-logo.png" alt="Control Hub" className="w-10 h-10" />
+        
           <h1 className="text-xl font-bold text-[#253847] font-sans mr-4 whitespace-nowrap">Flow Settings</h1>
           <div className="flex-1">
             <FilterBar onApply={() => {}} />
