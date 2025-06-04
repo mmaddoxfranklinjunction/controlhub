e95aeb678902e8c323adf8e10740b1b703e92a84
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-06-04 10:59:45 -0400

    Add files via upload

---
+++ src/pages/autoflows/WorkflowPanel.js
@@ -0,0 +1,24 @@
+// src/data/hostStoreList.js
+
+const hostStoreList = [
+  "All Hosts",
+  "BDP",
+  "CAT",
+  "CLD",
+  "DEN",
+  "ETA",
+  "EXP",
+  "GAD",
+  "GDE",
+  "GUM",
+  "Georgia Diner",
+  "LOB",
+  "MIL",
+  "NAN",
+  "ONE",
+  "RLB",
+  "VIS",
+  "andmarket"
+];
+
+export default hostStoreList;
