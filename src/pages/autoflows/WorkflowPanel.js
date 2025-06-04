5803b0888b51e38bf68529d56769691675ffee4e
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-06-04 11:19:24 -0400

    Update WorkflowPanel.js

--- src/pages/autoflows/WorkflowPanel.js
+++ src/pages/autoflows/WorkflowPanel.js
@@ -10,7 +10,7 @@ const WorkflowPanel = () => {
         {/* Logo + Title + FilterBar Row */}
         <div className="flex items-center gap-3 mb-4">
           <img src="/fj-circle-logo.png" alt="Control Hub" className="w-10 h-10" />
-          <h1 className="text-xl font-bold text-[#253847] font-sans mr-4 whitespace-nowrap">WorkFlow Panel</h1>
+          <h1 className="text-xl font-bold text-[#253847] font-sans mr-4 whitespace-nowrap">Control Panel</h1>
           <div className="flex-1">
             <FilterBar onApply={() => {}} />
           </div>
