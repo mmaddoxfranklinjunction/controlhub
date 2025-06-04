e49930dfce6a35a4c76c48773d93a1e70c535ebc
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-06-04 11:08:53 -0400

    Update WorkflowPanel.js

--- src/pages/autoflows/WorkflowPanel.js
+++ src/pages/autoflows/WorkflowPanel.js
@@ -1,24 +1,30 @@
-// src/data/hostStoreList.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageWrapper from '../../components/shared/PageWrapper';
+import FilterBar from '../../components/shared/FilterBar';
 
-const hostStoreList = [
-  "All Hosts",
-  "BDP",
-  "CAT",
-  "CLD",
-  "DEN",
-  "ETA",
-  "EXP",
-  "GAD",
-  "GDE",
-  "GUM",
-  "Georgia Diner",
-  "LOB",
-  "MIL",
-  "NAN",
-  "ONE",
-  "RLB",
-  "VIS",
-  "andmarket"
-];
+const WorkflowPanel = () => {
+  
 
-export default hostStoreList;
+
+  return (
+    <PageWrapper>
+      <div className="px-4 py-2">
+        {/* Logo + Title + FilterBar Row */}
+        <div className="flex items-center gap-3 mb-4">
+          <img src="/fj-circle-logo.png" alt="Control Hub" className="w-10 h-10" />
+          <h1 className="text-xl font-bold text-[#253847] font-sans mr-4 whitespace-nowrap">Control Panel</h1>
+          <div className="flex-1">
+            <FilterBar onApply={() => {}} />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+         
+        </div>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default WorkflowPanel;
