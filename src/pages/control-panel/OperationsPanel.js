028420d05491fb11cdc8ac7a961cc2efa42b99a3
mmaddoxfranklinjunction <96783513+mmaddoxfranklinjunction@users.noreply.github.com>
2025-06-04 13:48:24 -0400

    Update OperationsPanel.js

--- src/pages/control-panel/OperationsPanel.js
+++ src/pages/control-panel/OperationsPanel.js
@@ -15,24 +15,24 @@ const OperationsPanel = () => {
   
   return (
     <PageWrapper>
-        <div className="max-w-5xl mx-auto px-6 py-0">
+             <div className="px-5 py-5">
         {/* Title + Toggle */}
         <div className="flex items-center justify-between mt-0 mb-4">
-          <h1 className="text-2xl font-bold text-[#253847]">Operations Conrol Panel</h1>
+          <h1 className="text-xl font-bold text-[#253847]">Operations Control</h1>
           <div className="flex bg-[rgba(179,40,45,0.09)] rounded-full w-52 h-8 shadow-inner cursor-pointer text-xs border border-[#b3282d]">
             <button
               className={`flex-1 px-3 py-1 rounded-full transition font-bold
-                ${view === "insights" ? "bg-[#b3282d] text-white shadow" : "text-[#b3282d] bg-[rgba(179,40,45,0.09)]"}`}
-              style={{ fontSize: "13px", height: "32px", transition: "all 0.15s" }}
-              onClick={() => setView("insights")}
+                ${toggle === "insights" ? "bg-[#b3282d] text-white shadow" : "text-[#b3282d] bg-[rgba(179,40,45,0.09)]"}`}
+              style={{ fontSize: "12px", height: "25px", transition: "all 0.25s" }}
+              onClick={() => setToggle("insights")}
             >
               Insights
             </button>
             <button
               className={`flex-1 px-3 py-1 rounded-full transition font-bold
-                ${view === "controls" ? "bg-[#b3282d] text-white shadow" : "text-[#b3282d] bg-[rgba(179,40,45,0.09)]"}`}
-              style={{ fontSize: "13px", height: "32px", transition: "all 0.15s" }}
-              onClick={() => setView("controls")}
+                ${toggle === "controls" ? "bg-[#b3282d] text-white shadow" : "text-[#b3282d] bg-[rgba(179,40,45,0.09)]"}`}
+              style={{ fontSize: "12px", height: "25px", transition: "all 0.25s" }}
+              onClick={() => setToggle("controls")}
             >
               Controls
             </button>
